a75bc644727ae61b70e81f9b34b6a3269fa60968
silentDoc <ramon78t@yahoo.es>
2023-02-20 21:50:23 +0100

    Day 1 - Part 2 Completed. Finding the first duplicate

--- AoC18/Day01/DeviceCalibrator.cs
+++ AoC18/Day01/DeviceCalibrator.cs
@@ -7,7 +7,21 @@
         public void ParseInput(List<string> lines)
             => freqChanges = lines.Select(x => int.Parse(x.Trim())).ToList();
 
+        int FirstDuplicateFreq()
+        { 
+            HashSet<int> freqs = new HashSet<int>();
+            var current = 0;
+            var pos = 0;
+
+            while (freqs.Add(current))
+            {
+                current += freqChanges[pos % freqChanges.Count];
+                pos++;
+            }
+            return current;
+        }
+
         public int Solve(int part = 1)
-            => freqChanges.Sum();
+            => part == 1 ? freqChanges.Sum() : FirstDuplicateFreq();
     }
 }
