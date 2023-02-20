86415884c5e56a5163932c856b55c5a5194a88f2
silentDoc <ramon78t@yahoo.es>
2023-02-20 21:32:43 +0100

    Prepare solution structure

--- AoC18/Program.cs
+++ AoC18/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace AoC18
+﻿using System.Diagnostics;
+
+namespace AoC18
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int day = 1;
+            int part = 1;
+            bool test = !false;
+
+            string input = "./Input/day" + day.ToString("00");
+            input += (test) ? "_test.txt" : ".txt";
+
+            Console.WriteLine("AoC 2018 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+            Stopwatch st = new();
+            st.Start();
+            string result = day switch
+            {
+                1 => day1(input, part).ToString(),
+                _ => throw new ArgumentException("Wrong day number - unimplemented")
+            };
+            st.Stop();
+            Console.WriteLine("Result : {0}", result);
+            Console.WriteLine("Ellapsed : {0}", st.Elapsed.TotalSeconds);
+        }
+
+        static int day1(string input, int part)
+        {
+            return 0;
         }
     }
 }
