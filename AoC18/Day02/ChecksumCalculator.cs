981ee353c40724dfd5b49932111685dbe9a3b346
silentDoc <ramon78t@yahoo.es>
2023-02-23 21:44:07 +0100

    Day 2 - Part 2 Completed. Common letters

--- AoC18/Day02/ChecksumCalculator.cs
+++ AoC18/Day02/ChecksumCalculator.cs
@@ -11,7 +11,26 @@
         int CalcCheck(List<string> lines)
             =>  lines.Sum(x => FindOccurrences(x, 2)) * lines.Sum(x => FindOccurrences(x, 3));
 
+        int Hamming(string str1, string str2)
+            => str1.ToCharArray().Zip(str2.ToCharArray(), (x, y) => x == y).Count(r => r == false);
+
+        string FindCommonLetters(List<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                var rest = lines.Skip(lines.IndexOf(line) + 1).ToList();
+                var hamms = rest.Select(x => Hamming(line, x)).ToList();
+                if (hamms.IndexOf(1) != -1)
+                {
+                    var comp = rest[hamms.IndexOf(1)];
+                    var range = Enumerable.Range(0, comp.Length);
+                    return string.Join("", range.Select(x => comp[x] == line[x] ? comp[x].ToString() : ""));
+                }
+            }
+            return "";
+        }
+
         public string Solve(List<string> input, int part)
-            => CalcCheck(input).ToString();
+            => part == 1 ? CalcCheck(input).ToString() : FindCommonLetters(input);
     }
 }
