cb15dc8c7a25e8ffd762cca537589e36a5026fff
silentDoc <ramon78t@yahoo.es>
2023-02-24 12:43:20 +0100

    Day 4 - Part 1 Complete. The most sleepy

---
+++ AoC18/Day04/GuardWatcher.cs
@@ -0,0 +1,82 @@
+﻿namespace AoC18.Day04
+{
+    class Guard
+    {
+        public int Id;
+        public DateTime BeginShift;
+        public List<DateTime> FallAsleep;
+        public List<DateTime> WakeUp;
+
+        public Guard(int id, DateTime beginShift)
+        {
+            Id = id;
+            BeginShift = beginShift;
+            FallAsleep = new();
+            WakeUp = new();
+        }
+
+        public int SleepTime
+            => WakeUp.Zip(FallAsleep, (w, f) => w.Subtract(f).Minutes).Sum();
+
+        public int MostLikelyMinute()
+        {
+            Dictionary<int, int> sleepMap = new();
+            for (int i = 0; i < FallAsleep.Count; i++)
+                for (int m = FallAsleep[i].Minute; m < WakeUp[i].Minute; m++)
+                {
+                    if(!sleepMap.ContainsKey(m))
+                        sleepMap[m] = 0;
+                    sleepMap[m]++;
+                }
+            return sleepMap.Keys.Where(x => sleepMap[x] == sleepMap.Values.Max()).First();
+        }
+
+    }
+
+    internal class GuardWatcher
+    {
+        List<(DateTime when, string what)> sequence = new();
+        List<Guard> guards = new();
+        void ParseLine(string line)
+        { 
+            var parts = line.Split(']');
+            var dt = DateTime.Parse(parts[0].Substring(1));
+            sequence.Add( (dt, parts[1].Trim()));
+        }
+
+        public void ParseInput(List<string> lines)
+        {
+            lines.ForEach(ParseLine);
+            var sortedSeq = sequence.OrderBy(x => x.when).ToList();
+            var currentGuardId = -1;
+            foreach ( var entry in sortedSeq) 
+            {
+                var info = entry.what;
+                if (info.StartsWith("Guard #"))
+                {
+                    var strNum = info.Replace("Guard #", "").Replace(" begins shift", "");
+                    currentGuardId = int.Parse(strNum);
+                    guards.Add(new Guard(currentGuardId, entry.when));
+                }
+                else
+                {
+                    var guard = guards.First(x => x.Id == currentGuardId);
+                    if (info.StartsWith("falls"))
+                        guard.FallAsleep.Add(entry.when);
+                    else
+                        guard.WakeUp.Add(entry.when);
+                }
+            }
+        }
+
+        int CalcPart1()
+        {
+            var maxSleep = guards.Max(x => x.SleepTime);
+            var guard = guards.First(x => x.SleepTime == maxSleep);
+            return guard.Id * guard.MostLikelyMinute();
+        }
+
+        public int Solve(int part = 1)
+            => CalcPart1();
+    }
+}
--- AoC18/Program.cs
+++ AoC18/Program.cs
@@ -8,7 +8,7 @@ namespace AoC18
         {
             int day = 4;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -56,8 +56,10 @@ namespace AoC18
         static int day4(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
+            Day04.GuardWatcher watcher = new();
+            watcher.ParseInput(lines);
           
-            return 0;
+            return watcher.Solve(part);
         }
     }
 }
