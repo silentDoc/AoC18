5c7327ba8c596d1677c2acfb7e70618b687a39ec
silentDoc <ramon78t@yahoo.es>
2023-02-24 17:05:37 +0100

    Day 5 - Part 1. Optimization to prep part 2

--- AoC18/Day05/PolymerReductor.cs
+++ AoC18/Day05/PolymerReductor.cs
@@ -1,5 +1,4 @@
-﻿using MoreLinq;
-using System.Text;
+﻿using System.Text;
 
 namespace AoC18.Day05
 {
@@ -10,27 +9,28 @@ namespace AoC18.Day05
         public void ParseInput(List<string> lines)
             => polymer = lines[0];
 
-        IList<char>? FindMolecule(string polymer)
-            => polymer.Window(2).FirstOrDefault(x => ((char.IsUpper(x[0]) && char.IsLower(x[1])) || (char.IsUpper(x[1]) && char.IsLower(x[0])))
-                                                     && char.ToUpper(x[0]) == char.ToUpper(x[1]));
-
-        int Reduce(int part = 1)
+        int Reduce(string input)
         {
-            var sb = new StringBuilder(polymer);
-            // Perfect time to start playing with MoreLinQ library - windows !!!:)
-            var occurrence = FindMolecule(sb.ToString());
+            var sb = new StringBuilder(input);
+            int dif = Math.Abs('A' - 'a');
+            var currentPos = 0;
 
-            while (occurrence != null)
+            while(currentPos< sb.Length-1) 
             {
-                string molecule = new string(occurrence.ToArray());
-                int index = sb.ToString().IndexOf(molecule);
-                sb.Remove(index, 2);
-                occurrence = FindMolecule(sb.ToString());
+                if (Math.Abs(sb[currentPos] - sb[currentPos + 1]) == dif)
+                {
+                    sb.Remove(currentPos, 2);
+                    currentPos--;
+                    if (currentPos < 0)
+                        currentPos = 0;
+                }
+                else
+                    currentPos++;
             }
             return sb.Length;
         }
 
-        public int Solve(int part = 1)
-            => Reduce(part);
+       public int Solve(int part = 1)
+            => part == 1 ? Reduce(polymer): 0;
     }
 }
