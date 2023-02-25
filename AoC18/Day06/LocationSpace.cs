98545bed8e5e576a42c4951a0429e594fb7e0b68
silentDoc <ramon78t@yahoo.es>
2023-02-25 22:40:34 +0100

    Day 6 - Part 1 Completed. Finding the closest

---
+++ AoC18/Day06/LocationSpace.cs
@@ -0,0 +1,56 @@
+﻿using AoC18.Common;
+
+namespace AoC18.Day06
+{
+    internal class LocationSpace
+    {
+        List<Coord2D> locations = new();
+
+        void ParseLine(string line)
+        {
+            var coord = line.Split(",").Select(x => int.Parse(x)).ToArray();
+            locations.Add(new Coord2D(coord[0], coord[1]));
+        }
+
+        public void ParseInput(List<string> lines)
+            => lines.ForEach(ParseLine);
+
+        List<(Coord2D position, int distance)> GetDistances(Coord2D position)
+            => locations.Select((x, a) => (x, x.Manhattan(position))).ToList();
+
+        int LargestArea()
+        {
+            var minX = locations.Min(c => c.x);
+            var maxX = locations.Max(c => c.x);
+            var minY = locations.Min(c => c.y);
+            var maxY = locations.Max(c => c.y);
+
+            HashSet<Coord2D> infinityPoints = new();
+            Dictionary<Coord2D, int> amountOfClosests = new();
+
+            for (int x = minX; x <= maxX; x++)
+                for (int y = minY; y <= maxY;y++)
+                {
+                    var currentPoint = new Coord2D(x, y);
+                    var listDistances = GetDistances(currentPoint).OrderBy(x => x.distance).ToList(); 
+                    var minDistance = listDistances.Min(x => x.distance);
+
+                    var closest = listDistances.Where(x => x.distance == minDistance).ToList();
+                    if ( (x == minX || y == minY || x == maxX || y == maxY) && closest.Count() ==1)
+                        infinityPoints.Add(closest.First().position);
+
+                    if (closest.Count == 1)
+                    {
+                        var closestLocation = closest.Select(x => x.position).First();
+                        if (!amountOfClosests.ContainsKey(closestLocation))
+                            amountOfClosests[closestLocation] = 0;
+                        amountOfClosests[closestLocation]++;
+                    }
+                }
+            return amountOfClosests.Where(x => !infinityPoints.Contains(x.Key)).Max(x => x.Value);
+        }
+
+        public int Solve(int part = 1)
+            => LargestArea();
+    }
+}
--- AoC18/Program.cs
+++ AoC18/Program.cs
@@ -8,7 +8,7 @@ namespace AoC18
         {
             int day = 6;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -75,8 +75,10 @@ namespace AoC18
         static int day6(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-           
-            return 0;
+            Day06.LocationSpace space = new();
+            space.ParseInput(lines);
+                      
+            return space.Solve(part);
         }
     }
 }
