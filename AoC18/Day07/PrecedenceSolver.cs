c392911faacbea0b6c8e7a6b4c2f3340927d1c5f
silentDoc <ramon78t@yahoo.es>
2023-02-27 13:24:44 +0100

    Day 7 - Part 2 Completed. Teamwork

--- AoC18/Day07/PrecedenceSolver.cs
+++ AoC18/Day07/PrecedenceSolver.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 
 namespace AoC18.Day07
 {
@@ -40,10 +41,10 @@ namespace AoC18.Day07
         public void ParseInput(List<string> lines)
             => lines.ForEach(ParseLine);
 
-        string FindOrder(int part = 1)
+        string FindOrder()
         {
             // I will implement this one iteratively instead of using recursion, for a change
-            StringBuilder nodeOder = new();
+            StringBuilder nodeOrder = new();
             while (nodes.Any(x => x.Solved == false))
             {
                 var iterationSet = nodes.Where(x => x.CanBeSolved && !x.Solved).OrderBy(x => x.Id).ToList();
@@ -51,13 +52,59 @@ namespace AoC18.Day07
                 if (node != null) 
                 {
                     node.Solved = true;
-                    nodeOder.Append(node.Id);
+                    nodeOrder.Append(node.Id);
                 }
             }
-            return nodeOder.ToString();
+            return nodeOrder.ToString();
+        }
+
+        string TeamWork()
+        {
+            List<int> releaseSecond = new();
+            List<char> nodeBeingProcessed = new();
+            int tick = 0;
+            const int Num_Workers = 5;  
+            const int Cycle_Add = 60;
+
+            StringBuilder nodeOrder = new();
+            while (nodes.Any(x => x.Solved == false))
+            {
+                // Check if the work has finished
+                if (releaseSecond.Any(x => x == tick))
+                {
+                    var releases = releaseSecond.Where(x => x == tick).ToList();
+                    foreach(var release in releases) 
+                    {
+                        var ind = releaseSecond.IndexOf(tick);
+                        var nodeId = nodeBeingProcessed[ind];
+                        nodes.First(x => x.Id == nodeId).Solved = true;
+                        releaseSecond.RemoveAt(ind);
+                        nodeBeingProcessed.RemoveAt(ind);
+                        nodeOrder.Append(nodeId);
+                    }
+                }
+                
+                // Check if we can start solving something
+                var iterationSet = nodes.Where(x => x.CanBeSolved && !x.Solved && !nodeBeingProcessed.Contains(x.Id)).OrderBy(x => x.Id).ToList();
+                var avaialableElves = Math.Max(0, Num_Workers - nodeBeingProcessed.Count);
+                var numToSolve = Math.Min(iterationSet.Count, avaialableElves);
+
+                for (int i = 0; i < numToSolve; i++)
+                {
+                    char id = iterationSet[i].Id;
+                    nodeBeingProcessed.Add(id);
+                    int relSecond = Cycle_Add + (id - 'A' + 1);
+                    releaseSecond.Add(tick+relSecond);
+                }
+                if (releaseSecond.Count() == 0)
+                    break;
+                else
+                    tick = releaseSecond.Min();
+            }
+            return tick.ToString();
         }
 
         public string Solve(int part = 1)
-            => FindOrder(part);
+            => part ==1 ? FindOrder() : TeamWork();
     }
 }
