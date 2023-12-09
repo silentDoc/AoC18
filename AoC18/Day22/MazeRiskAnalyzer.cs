6f8eb0d5d37fbb794a4e67f451c51cf82daa9be2
silentDoc <ramon78t@yahoo.es>
2023-12-09 13:16:27 +0100

    Day 22 - Part 1 Completed. Caves and Modulos

---
+++ AoC18/Day22/MazeRiskAnalyzer.cs
@@ -0,0 +1,58 @@
+﻿using AoC18.Common;
+using System.Drawing;
+
+namespace AoC18.Day22
+{
+    class MazeRiskAnalyzer
+    {
+        Dictionary<Coord2D, int> GeoIndex  = new();
+        Dictionary<Coord2D, int> ErosionLevels = new();
+        Dictionary<Coord2D, int> Types = new();
+        int Depth = 0;
+        Coord2D Target = new(0, 0);
+        Coord2D up = new Coord2D(0, -1);
+        Coord2D left = new Coord2D(-1, 0);
+
+        public void ParseInput(List<string> lines)
+        {
+            Depth = int.Parse(lines[0].Replace("depth:", ""));
+            var target = lines[1].Replace("target:", "").Split(',').Select(x => int.Parse(x)).ToList();
+            Target = new Coord2D(target[0], target[1]);
+        }
+
+        int CellRisk(Coord2D coord)
+        {
+            var retVal = coord switch
+            {
+                (0, 0) => 0,
+                (_, 0) => coord.x * 16807,
+                (0, _) => coord.y * 48271,
+                (_, _) => ErosionLevels[coord + up] * ErosionLevels[coord + left]
+            };
+            return retVal;
+        }
+
+        int Modulo(int a, int b)
+            => (a % b + b) % b;
+
+        int SolveMaze(int part =1)
+        {
+            for (int i = 0; i <= Target.x; i++)
+                for (int j = 0; j <= Target.y; j++)
+                {
+                    GeoIndex[(i, j)] = CellRisk((i, j));
+                    ErosionLevels[(i, j)] = Modulo(GeoIndex[(i, j)] + Depth, 20183);
+                    Types[(i, j)] = ErosionLevels[(i, j)] % 3;
+                }
+
+            GeoIndex[Target] = 0;
+            ErosionLevels[Target] = Modulo(GeoIndex[Target] + Depth, 20183);
+            Types[Target] = ErosionLevels[Target] % 3;
+
+            return Types.Values.Sum();
+        }
+
+        public int Solve(int part = 1)
+            => SolveMaze(part);
+    }
+}
--- AoC18/Program.cs
+++ AoC18/Program.cs
@@ -8,7 +8,7 @@ namespace AoC18
         {
             int day = 22;
             int part = 1;
-            bool test = !false;
+            bool test = false;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -230,8 +230,10 @@ namespace AoC18
         static string day22(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
+            Day22.MazeRiskAnalyzer mazer = new();
+            mazer.ParseInput(lines);
 
-            return "";
+            return mazer.Solve(part).ToString();
         }
 
     }
